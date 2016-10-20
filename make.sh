d68b888f53c5ca48a3f11852b3ca807fc21f721a
jamesread <contact@jwread.com>
2016-10-20 07:53:53 +0100

    .

--- make.sh
+++ make.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -x
 
 buildid -n 
 mkdir -p pkg
 mkdir -p dist/upsilon-serviceChecks
 cp -r src pkg dist/upsilon-serviceChecks
 cd dist
+find
 zip upsilon-serviceChecks.zip upsilon-serviceChecks
