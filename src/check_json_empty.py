213bfc34838629b253f03b2f319793f40cf29c47
jamesread <contact@jwread.com>
2017-01-23 15:31:00 +0000

    Few syntax errors

--- src/check_json_empty.py
+++ src/check_json_empty.py
@@ -6,14 +6,21 @@ from upsilon.serviceHelpers import *
 
 parser = argparse.ArgumentParser();
 parser.add_argument("baseurl")
-parser.add_argument("-c", "--criticalCount", type = int, default = 0)
-parser.add_argument("-w", "--warningCount", type = int, default = 5)
+parser.add_argument("-c", "--countCritical", type = int, default = 0)
+parser.add_argument("-w", "--countWarning", type = int, default = 5)
 args = parser.parse_args()
 
 import json
 
 content = urllib2.urlopen(args.baseurl).read()
-jsonStructure = json.decoder(content);
+
+try:
+    jsonStructure = json.loads(content);
+except: 
+    print "Could not parse JSON"
+    print content
+
+    exitCritical()
 
 if len(jsonStructure) > args.countCritical:
     exitCritical()
