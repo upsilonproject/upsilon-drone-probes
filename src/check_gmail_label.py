32eabd4dbb6280fab6fc0d0c19c13994fb10c251
jamesread <contact@jwread.com>
2017-03-22 13:48:52 +0000

    eval messages

--- src/check_gmail_label.py
+++ src/check_gmail_label.py
@@ -3,11 +3,15 @@
 from __future__ import print_function
 import httplib2
 import os
+import json
 
-from apiclient import discovery
+from googleapiclient import discovery, http
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+import time
+from datetime import datetime
+import time
 
 import argparse
 
@@ -17,6 +21,7 @@ parser = argparse.ArgumentParser(parents=[tools.argparser])
 parser.add_argument('labels', nargs = '*', default = []);
 parser.add_argument('--csv', action = 'store_true');
 parser.add_argument('--metricsTotal', action = 'store_true');
+parser.add_argument('--evalMessages', action = 'store_true');
 parser.add_argument('--countCritical', default = 30)
 parser.add_argument('--countWarning', default = 5)
 parser.add_argument('--clientSecretFile', default = 'client_secrets.json')
@@ -64,12 +69,79 @@ def getKarma(karma):
 
     return "GOOD"
 
-def main():
-    """Shows basic usage of the Gmail API.
+def onMessage(req, resp, err):
+    global evaledThreads
+    evaledThreads.append(resp)
 
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
-    """
+evaledThreads = []
+
+def evalMessages(service, label):
+    fetchLabelIds = [label['id']]
+
+    print("evalMessages, fetch label IDs:", fetchLabelIds)
+
+    response = service.users().threads().list(userId = 'me', labelIds = fetchLabelIds).execute()
+    threads = []
+
+    if "threads" in response:
+        threads.extend(response['threads'])
+
+    while "nextPageToken" in response:
+        page_token = response['nextPageToken']
+        response = service.users().threads().list(userId = 'me', labelIds = fetchLabelIds, pageToken = page_token).execute()
+        threads.extend(response['threads'])
+        print("getting more messages:" + str(len(threads)))
+
+    batch = service.new_batch_http_request();
+
+    for thread in threads:
+        batch.add(service.users().threads().get(userId = 'me', id = thread['id'], format = "minimal"), callback=onMessage)
+
+    global evaledThreads
+    evaledThreads = []
+
+    batch.execute();
+
+    print("finished eval messages")
+
+    timestamps = []
+    for thread in evaledThreads:
+        latestTimestamp = 0
+
+        if thread == None or thread['messages'] == None:
+            continue;
+
+        for message in thread['messages']:
+            if message['internalDate'] > latestTimestamp:
+                latestTimestamp = int(message['internalDate']) / 1000
+
+        timestamps.append(datetime.fromtimestamp(latestTimestamp))
+
+    return timestamps
+
+def addMetricsForTimestamps(metadata, labelName, threadTimestamps):
+    now = datetime.today()
+
+    lastWeek = []
+    lastMonth = []
+    older = []
+
+    for timestamp in threadTimestamps:
+        diff = now - timestamp
+
+        if diff.days < 7:
+            lastWeek.append(timestamp)
+        if diff.days < 30:
+            lastMonth.append(timestamp)
+        else:
+            older.append(timestamp)
+
+    metadata.addMetric(labelName + ' in the last week', len(lastWeek), 'UNKNOWN')
+    metadata.addMetric(labelName + ' in the last month', len(lastMonth), 'UNKNOWN')
+    metadata.addMetric(labelName + '_older', len(older), 'UNKNOWN')
+    print("added metrics")
+
+def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
@@ -79,27 +151,33 @@ def main():
 
         labels = results.get('labels', [])
     else:
-        labels = args.labels
+        labels = []
 
-    print(labels)
+        for label in args.labels:
+            labels.append({'id': label})
 
     metadata = serviceHelpers.clsmetadata()
 
     if not labels:
         print('No labels found.')
-    else:
-      print('Labels:')
-      for label in labels:
-        label_info = service.users().labels().get(id = label, userId = 'me').execute()
+        return
+
+    for label in labels:
+        label_info = service.users().labels().get(id = label['id'], userId = 'me').execute()
 
         if args.csv:
-            print(label_info['name'], ",", label_info['threadsTotal'], ",", label_info['threadsUnread'])
+            print(label_info['name'], ",", label_info['threadsUnread'], ",", label_info['threadsTotal'])
         else:
             print(label_info['name'], label_info['threadsUnread'], '/', label_info['threadsTotal'])
 
         if args.metricsTotal: 
             metric = metadata.addMetric(label_info['id'] + '_total', label_info['threadsTotal'], getKarma(label_info['threadsTotal']))
             metric['caption'] = label_info['name'] + ' Total'
+
+        if args.evalMessages:
+            threadTimestamps = evalMessages(service, label);
+
+            addMetricsForTimestamps(metadata, label_info['name'], threadTimestamps)
 
     serviceHelpers.exitOk(metadata);
 
