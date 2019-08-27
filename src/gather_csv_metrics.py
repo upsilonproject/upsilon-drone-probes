ea95c04bf0b7919bcc9cd7f3c754f77bef22624f
jamesread <contact@jwread.com>
2019-08-27 09:26:16 +0000

    headers

--- src/gather_csv_metrics.py
+++ src/gather_csv_metrics.py
@@ -13,9 +13,16 @@ metadata = clsmetadata();
 with open(args.filename) as csvfile:
     reader = csv.reader(csvfile)
 
+    lines = []
+
     for line in reader:
-        pass
-    
-    metadata.addMetric("valueFromCsv", line[0])
+        lines.append(line)
+
+    headers = lines[0:1][0]
+    recentValues = lines[-1:][0]
+
+    for index in range(len(headers)):
+        metadata.addMetric(headers[index].strip(), recentValues[index].strip())
+
 
 exit(metadata = metadata)
