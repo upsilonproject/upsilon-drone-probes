8722f59b9ce24e16dde8f2e6992b49184fbf6e85
jamesread <contact@jread.com>
2022-12-01 11:25:52 +0000

    time

--- src/check_discover_self.py
+++ src/check_discover_self.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 from upsilon.service import ServiceController, easyexec
+import datetime
 
 srv = ServiceController()
 
 release, _ = easyexec(["cat", "/etc/redhat-release"])
 srv.addSubresult("release", value = release)
+srv.addSubresult('date', value=str(datetime.datetime.now()))
 srv.exitOk()
